df9a6752ccdd59e7313f9ca3da5f4b088dec0089
misterchaos <kobe524348@gmail.com>
2019-07-28 19:04:51 +0800

    fix:修复PageUtil计算错误的问题

--- src/main/java/com/qgailab/util/PageUtils.java
+++ src/main/java/com/qgailab/util/PageUtils.java
@@ -1,12 +1,13 @@
 package com.qgailab.util;
 
 /**
- * @description 返回页面数
  * @author < a href=" ">郭沛</ a>
+ * @description 返回页面数
  * @date 2019-07-28 18:09
  */
 public class PageUtils {
     public static int getPage(int count, int pageSize) {
-      return (int) Math.ceil(count / pageSize);
+        int page = (int) Math.ceil((float)count /(float) pageSize);
+        return page;
     }
 }
