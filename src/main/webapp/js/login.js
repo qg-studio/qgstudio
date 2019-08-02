226653808babba74ddeb2a2d8a9d0ed842994b00
misterchaos <kobe524348@gmail.com>
2019-08-02 21:14:46 +0800

    fix:修复登陆页面无法跳页面的问题

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -1,33 +1,31 @@
-define(['require', 'tools'], function(require) {
+define(['require', 'tools'], function (require) {
     var tools = require('tools');
 
-    $(function() {
-        $("button:eq(0)").click(function() {
-            var data ={
-            	username: $("input:eq(0)").val(),
-            	password: $("input:eq(1)").val()
+    $(function () {
+        $("button:eq(0)").click(function () {
+            var data = {
+                username: $("input:eq(0)").val(),
+                password: $("input:eq(1)").val()
             };
-            if(!tools.verificat.isNotNullTrim(data.username) || !tools.verificat.isNotNullTrim(data.password)) {
-                alert("输入不能为空！");          
+            if (!tools.verificat.isNotNullTrim(data.username) || !tools.verificat.isNotNullTrim(data.password)) {
+                alert("输入不能为空！");
             } else {
                 $.ajax({
-        			type: "POST",
-        			url: "http://www.cxkball.club:2333/user/login",
-        			dataType: "json",
-        			contentType: "application/json",
-        			data: JSON.stringify(data),
-        			success: function(data) {
-        				if (data.message == "account_not_found") {
-        					alert("该账户不存在！");
-        				} else if (data.message == "password_incorrect") {
-        					alert("密码输入错误！");
-        				} else if(data.message == "success") {
-        					window.location.href="http://www.cxkball.club:2333/about-qg.html";
-        				}
-        			} 
-        		})
+                    type: "POST",
+                    url: "http://www.cxkball.club:2333/user/login",
+                    dataType: "json",
+                    contentType: "application/json",
+                    data: JSON.stringify(data),
+                    success: function (data) {
+                        if (data.status == 200) {
+                            window.location.href = "http://www.cxkball.club:2333/about-qg.html";
+                        }else {
+                            alert(data.message);
+                        }
+                    }
+                })
             }
-          
+
         });
     });
 
