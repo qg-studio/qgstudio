1fd342ab264338e986f38baa3754e3199d408711
misterchaos <kobe524348@gmail.com>
2019-08-03 08:25:28 +0800

    feat:增加了退出登陆功能，并且增加用户名格式校验

--- src/main/java/com/qgailab/controller/UserController.java
+++ src/main/java/com/qgailab/controller/UserController.java
@@ -3,12 +3,10 @@ package com.qgailab.controller;
 import com.qgailab.model.dto.ServiceResult;
 import com.qgailab.model.po.User;
 import com.qgailab.service.UserService;
+import com.qgailab.service.constants.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -34,5 +32,18 @@ public class UserController {
             request.getSession().setAttribute("login",login);
         }
         return result;
+    }
+
+    @RequestMapping(value = "/logout", method = RequestMethod.POST)
+    public @ResponseBody
+    ServiceResult login(HttpServletRequest request) {
+        try {
+            request.getSession().removeAttribute("login");
+            request.getSession().invalidate();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ServiceResult(500,Message.please_retry);
+        }
+        return new ServiceResult(200, Message.success);
     }
 }
